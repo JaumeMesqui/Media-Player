227e2795ad024be48c5fa87ff6e7ebbbf4ac1756
Maria Orell Monserrat <mariaorell2000@gmail.com>
2022-03-25 23:47:35 +0100

    més canvis css editor

--- js/scriptEditor.js
+++ js/scriptEditor.js
@@ -2,21 +2,22 @@
 var editorPrincipal = document.createElement("div");
 editorPrincipal.setAttribute('class', "contigutEditor");
 
-//titol principal formulari
+//opcio principal per crear un nou vtt
 var crearNouVtt = document.createElement("p");
-crearNouVtt.setAttribute('class', "titolFormulari");
+crearNouVtt.setAttribute('class', "opcioPrincipalEditor");
 var text3 = document.createTextNode("Crear un nou VTTs");
 crearNouVtt.appendChild(text3);
 editorPrincipal.appendChild(crearNouVtt);
 
-//botó per obrir formulari editor
-var afegirCue = document.createElement("button");
-afegirCue.setAttribute('type', "button");
-afegirCue.setAttribute('class', "botonsEditar");
-var text1 = document.createTextNode('+');
-afegirCue.appendChild(text1);
+//botó per obrir editor de crear nou vtt
+var afegirVtt = document.createElement("button");
+afegirVtt.setAttribute('type', "button");
+afegirVtt.setAttribute('class', "botonsEditar");
+var iconoPlus = document.createElement("i");
+iconoPlus.setAttribute('class', "fa-solid fa-plus");
+afegirVtt.appendChild(iconoPlus);
 
-editorPrincipal.appendChild(afegirCue);
+editorPrincipal.appendChild(afegirVtt);
 
 
 //Caja de colores HTML y CSS con borde redondeado a la izquierda: llista VTTs
@@ -31,70 +32,122 @@ for (var i = 0; i < 3; i++) {
     var editarVtt = document.createElement("button");
     editarVtt.setAttribute('type', "button");
     editarVtt.setAttribute('class', "botonsEditar");
-    var text6 = document.createTextNode('E');
-    editarVtt.appendChild(text6);
-
+    var iconoEditar = document.createElement("i");
+    iconoEditar.setAttribute('class', "fa-solid fa-pencil");
+    editarVtt.appendChild(iconoEditar);
     editorPrincipal.appendChild(editarVtt);
+    
+    editarVtt.addEventListener("click", (e) => {
+        document.getElementById("editor").removeChild(editorPrincipal);
+        document.getElementById("editor").appendChild(editorSecundariCues);
+    });
 
      //botó per borrar un vtt
      var borrarVtt = document.createElement("button");
      borrarVtt.setAttribute('type', "button");
      borrarVtt.setAttribute('class', "botonsEditar");
-     var text7 = document.createTextNode('B');
-     borrarVtt.appendChild(text7);
- 
+     var iconoBasura = document.createElement("i");
+     iconoBasura.setAttribute('class', "fa-solid fa-trash-can");
+     borrarVtt.appendChild(iconoBasura);
      editorPrincipal.appendChild(borrarVtt);
 }
-
-
-
-
-/*
-//botó per tancar formulari editor
-var eliminarCue = document.createElement("button");
-eliminarCue.setAttribute('type',"button"); 
-eliminarCue.setAttribute('class',"botonsEditar"); 
-var text2 = document.createTextNode('Eliminar CUE');
-eliminarCue.appendChild(text2);
-
-editorPrincipal.appendChild(eliminarCue);*/
-
-
 
 //Afegir els botons de l'editor a l'Index
 document.getElementById("editor").appendChild(editorPrincipal);
 
+
 /* ************************************ */
 //Creació del elements del formulari
+
+var editorSecundariCues = document.createElement("div");
+editorSecundariCues.setAttribute('class', "contigutEditor");
+
 var formulari = document.createElement("form");
 formulari.setAttribute('action', "");
 
-var titolForm = document.createElement("h2");
-var text4 = document.createTextNode('FORMULARI');
-titolForm.appendChild(text4);
-formulari.appendChild(titolForm);
+var titolEditorCues = document.createElement("h2");
+var text7 = document.createTextNode("DADES SOBRE EL VTT SELECCIONAT");
+titolEditorCues.appendChild(text7);
+formulari.appendChild(titolEditorCues);
 
 var form1 = document.createElement("input");
 form1.setAttribute('type', "text");
-form1.setAttribute('nombre', "Cantant");
-form1.setAttribute('placeholder', "Cantant");
+form1.setAttribute('nombre', "nomArxiu");
+form1.setAttribute('placeholder', "Nom d'arxiu");
 formulari.appendChild(form1);
 
 var form2 = document.createElement("input");
 form2.setAttribute('type', "text");
-form2.setAttribute('nombre', "MinInici");
-form2.setAttribute('placeholder', "Minut d'inici");
+form2.setAttribute('nombre', "tipusVTT");
+form2.setAttribute('placeholder', "Tipus de VTT");
 formulari.appendChild(form2);
 
 var form3 = document.createElement("input");
 form3.setAttribute('type', "text");
-form3.setAttribute('nombre', "MinFinal");
-form3.setAttribute('placeholder', "Minut final");
+form3.setAttribute('nombre', "dadaX");
+form3.setAttribute('placeholder', "dadaX");
 formulari.appendChild(form3);
+
+editorSecundariCues.appendChild(formulari);
+
+//opcio principal editor
+var crearNouCue = document.createElement("p");
+crearNouCue.setAttribute('class', "opcioPrincipalEditor");
+var text6 = document.createTextNode("Crear una nova CUE");
+crearNouCue.appendChild(text6);
+editorSecundariCues.appendChild(crearNouCue);
+
+//botó per crear una nova cue
+var afegirCue = document.createElement("button");
+afegirCue.setAttribute('type', "button");
+afegirCue.setAttribute('class', "botonsEditar");
+var iconoPlus = document.createElement("i");
+iconoPlus.setAttribute('class', "fa-solid fa-plus");
+afegirCue.appendChild(iconoPlus);
+editorSecundariCues.appendChild(afegirCue);
+
+//llista cue's existent dins l'VTT seleccionat
+for (var i = 0; i < 3; i++) {
+    var elemLlistaCues = document.createElement("p");
+    elemLlistaCues.setAttribute('class', "llistaForm");
+    var text7 = document.createTextNode("CUE 1: subtítols en idioma X");
+    elemLlistaCues.appendChild(text7);
+    editorSecundariCues.appendChild(elemLlistaCues);
+
+    //botó per editar un vtt
+    var editarCue = document.createElement("button");
+    editarCue.setAttribute('type', "button");
+    editarCue.setAttribute('class', "botonsEditar");
+    var iconoEditar = document.createElement("i");
+    iconoEditar.setAttribute('class', "fa-solid fa-pencil");
+    editarCue.appendChild(iconoEditar);
+    editorSecundariCues.appendChild(editarCue);
+    
+    editarCue.addEventListener("click", (e) => {
+        document.getElementById("editor").removeChild(editorSecundariCues);
+        document.getElementById("editor").appendChild(editorPrincipal);
+        
+    });
+
+     //botó per borrar un vtt
+     var borrarCue = document.createElement("button");
+     borrarCue.setAttribute('type', "button");
+     borrarCue.setAttribute('class', "botonsEditar");
+     var iconoBasura = document.createElement("i");
+     iconoBasura.setAttribute('class', "fa-solid fa-trash-can");
+     borrarCue.appendChild(iconoBasura);
+     editorSecundariCues.appendChild(borrarCue);
+}
 
 var seccioBotons = document.createElement("div");
 seccioBotons.setAttribute('class', "seccioBotonsFormularis");
 
+var botoCancelar = document.createElement("input");
+botoCancelar.setAttribute('type', "button");
+botoCancelar.setAttribute('value', "Cancelar");
+botoCancelar.setAttribute('class', "botoFormulari");
+botoCancelar.setAttribute('id', "botonCancelar");
+seccioBotons.appendChild(botoCancelar);
 
 var botoAcceptar = document.createElement("input");
 botoAcceptar.setAttribute('type', "button");
@@ -103,23 +156,21 @@ botoAcceptar.setAttribute('id', "botoAcceptar");
 botoAcceptar.setAttribute('class', "botoFormulari");
 seccioBotons.appendChild(botoAcceptar);
 
-var botoCancelar = document.createElement("input");
-botoCancelar.setAttribute('type', "button");
-botoCancelar.setAttribute('value', "Cancelar");
-botoCancelar.setAttribute('class', "botoFormulari");
-botoCancelar.setAttribute('id', "botonCancelar");
-seccioBotons.appendChild(botoCancelar);
-formulari.appendChild(seccioBotons);
+editorSecundariCues.appendChild(seccioBotons);
 
 
-//Funció per afegir el formulari a l'Index
-afegirCue.addEventListener("click", (e) => {
-    document.getElementById("formEditor").appendChild(formulari);
+//Funció per obrir el formulari quan clicam al botó "+" o al botó d'editar "llapis"
+afegirVtt.addEventListener("click", (e) => {
+    document.getElementById("editor").removeChild(editorPrincipal);
+    document.getElementById("editor").appendChild(editorSecundariCues);
 });
 
+
+
 //Funció per eliminar el formulari de l'Index
-eliminarCue.addEventListener("click", (e) => {
-    document.getElementById("formEditor").removeChild(formulari);
+botoCancelar.addEventListener("click", (e) => {
+    document.getElementById("editorCues").removeChild(editorSecundariCues);
+    document.getElementById("editor").appendChild(editorPrincipal);
 });
 
 
